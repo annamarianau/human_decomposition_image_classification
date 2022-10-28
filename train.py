06d1ab7892c891694fde18a5cc76cadbdd46842a
annamarianau <anau@vols.utk.edu>
2022-10-28 11:50:36 -0500

    modified config and train.py

--- train.py
+++ train.py
@@ -10,10 +10,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+
+tf.random.set_seed(123)
 
 # build input pipeline 
-def load_preprocess_data(config):
-    file_w_paths = config['DATASET']['root_dataset']
+def load_preprocess_data(config, file_w_paths):
 
     not_found = []
     data = []
@@ -37,13 +39,15 @@ def load_preprocess_data(config):
             
             except:
                 not_found.append(row)
+            
             counter += 1
-            if counter == 100:
+            if counter == 64:
                 break
-    
-    print('Images not found: ', not_found)
+    print('Images not found: ', not_found) 
 
-    return data, labels
+    labels_cat = tf.keras.utils.to_categorical(labels, num_classes=config['DATASET']['num_class'])
+    
+    return data, labels_cat
 
     
 if __name__ == '__main__':
@@ -55,14 +59,20 @@ if __name__ == '__main__':
     
     # load and preprocess data
     print('Loading and preprocessing data...')
-    data, labels = load_preprocess_data(config) 
-    
-    # split into train and validation sets
-    X_train, X_val, y_train, y_val = train_test_split(data, labels,
-                                        random_state=123, shuffle=True,
-                                        test_size=0.3)
+    X_train, y_train = load_preprocess_data(config, config['DATASET']['train_path']) 
+    X_val, y_val = load_preprocess_data(config, config['DATASET']['val_path'])
+
     print(len(X_train), len(y_train), len(X_val), len(y_val))
-    
+
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+    X_val = np.array(X_val)
+    y_val = np.array(y_val)
+
+    print(X_train.shape, y_train.shape, X_val.shape, y_val.shape)
+
+    #sys.exit(0)
+
     data_augmentation = tf.keras.Sequential([
     # preprocessing layer which randomly flips images during training.
     tf.keras.layers.RandomFlip('horizontal_and_vertical'),
@@ -74,31 +84,105 @@ if __name__ == '__main__':
     if model_name == 'resnet50':
         base_model = tf.keras.applications.ResNet50(include_top = False, weights='imagenet', 
                                             input_shape = (config['DATASET']['img_size'], 
-                                                            config['DATASET']['img_size'],3),
-                                            pooling = 'avg')
-
+                                                           config['DATASET']['img_size'],3))
+    # freeze base model
     base_model.trainable = False
-
+    
+    # define model architecture
     inp = tf.keras.Input((config['DATASET']['img_size'], config['DATASET']['img_size'], 3))
     x = data_augmentation(inp)
     x = base_model(x, training=False)
-    #x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(256, activation='relu')(x)
-    x = tf.keras.layers.Dropout(0.1)(x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    #x = tf.keras.layers.Dense(256, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
     out = tf.keras.layers.Dense(config['DATASET']['num_class'], activation='softmax')(x)
 
     model = tf.keras.Model(inp, out)
     
     # compile the model
-    model.compile(optimizer='Adam',loss='categorical_crossentropy', metrics=['accuracy']) 
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+                loss='categorical_crossentropy',
+                metrics=['accuracy']) 
     print(model.summary())
     
-    # create checkpoint
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(model_name + '_epoch_{epoch:03d}_acc_{acc:03f}_val_acc_{val_acc:.5f}.h5', 
-                verbose=1, 
-                monitor='val_acc', 
-                save_best_only=True, 
-                mode='auto')  
+    # create checkpoint and early stopping 
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(
+                filepath='checkpoints/' + model_name + '-{epoch:03d}-{accuracy:03f}-{val_accuracy:.5f}',
+                monitor='val_accuracy',
+                verbose=0,
+                save_best_only=True,
+                save_weights_only=True,
+                mode='auto',
+                save_freq='epoch')  
 
+    early_stop = tf.keras.callbacks.EarlyStopping(
+                monitor='val_loss', 
+                patience=10,
+                mode='min', 
+                verbose=1) 
     
+    
+    # train model
+    print('Training model...')
+    batch_size = config['TRAIN']['batch_size']
+    num_epoch = 3 #config['TRAIN']['num_epoch']
+    train_steps = X_train.shape[0] // batch_size
+    val_steps = X_val.shape[0] // batch_size
+    
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    print('num_epochs:', num_epoch, 'batch_size:', batch_size, 'train_steps:', train_steps, 
+            'val_steps:', val_steps)
+
+    # fit model
+    history = model.fit(X_train,
+                        y_train,
+                        steps_per_epoch=train_steps,
+                        validation_data=(X_val, y_val),
+                        validation_steps=val_steps,
+                        batch_size=batch_size,
+                        epochs=num_epoch,
+                        callbacks=[checkpoint, early_stop],
+                        verbose=1) 
+   
+ 
+    tf.keras.models.save_model(model,
+                               filepath=config['MODEL']['model_path'])
+    
+    sys.exit(0)
+    
+    # model performance 
+    # store results
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    # plot results
+    # accuracy
+    plt.figure(figsize=(10, 16))
+    plt.rcParams['figure.figsize'] = [16, 9]
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['axes.grid'] = True
+    plt.rcParams['figure.facecolor'] = 'white'
+    plt.subplot(2, 1, 1)
+    plt.plot(acc, label='Training Accuracy')
+    plt.plot(val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.ylabel('Accuracy')
+    plt.title(f'Training and Validation Accuracy')
+    plt.savefig('plots/'+model_name+'_acc')
+ 
+    # loss
+    plt.figure(figsize=(10, 16))
+    plt.plot(loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.ylabel('Cross Entropy')
+    plt.title(f'Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.tight_layout(pad=3.0)
+    plt.savefig('plots/'+model_name+'_loss')
+
 
