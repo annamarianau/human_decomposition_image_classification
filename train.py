c364272032412fe52de39d8408dc5a5468493b90
annamarianau <anau@vols.utk.edu>
2023-03-23 09:26:23 -0500

    train.py now does 1st and 2nd step TL

--- train.py
+++ train.py
@@ -78,11 +78,11 @@ if __name__ == '__main__':
     
     # load and process data, or just load if it has been previously processed and serialized
     if process_data == 'y':
-        print('Loading and processing data from: ', config['DATASET']['data_path'])
+        print('########### Loading and processing data from:', config['DATASET']['data_path'], '###########')
         X_train, y_train = preprocess_data(config, config['DATASET']['train_path']) 
         X_val, y_val = preprocess_data(config, config['DATASET']['val_path'])
     elif process_data == 'n':
-        print('Loading data from: ', config['DATASET']['data_path'])
+        print('########### Loading data from: ', config['DATASET']['data_path'], '###########')
         f = open(config['DATASET']['train_path']+'.pickle', 'rb')
         train_data = pickle.load(f)
         X_train = train_data[0]
@@ -99,92 +99,79 @@ if __name__ == '__main__':
     X_val = np.array(X_val)
     y_val = np.array(y_val)
     print('X_train.shape:',X_train.shape,'y_train.shape:', y_train.shape)
-    print('X_val.shape:', X_val.shape,'y_val.shape:', y_val.shape)
-    
+    print('X_val.shape:', X_val.shape,'y_val.shape:', y_val.shape)    
+
+
     # create data augmentation layer
     if config['TRAIN']['augment'] == True:
         data_augmentation = tf.keras.Sequential([
         # preprocessing layer which randomly flips images during training.
-        #tf.keras.layers.RandomFlip('horizontal_and_vertical'),
+        tf.keras.layers.RandomFlip('horizontal_and_vertical'),
         # preprocessing layer which randomly rotates images during training
         tf.keras.layers.RandomRotation(0.2)])
     
     # create base model or load pretrained model from step 1 to be tuned
     model_name = config['MODEL']['name'] 
-    if model_name == 'resnet50':
+    if 'resnet50'in model_name:
         base_model = tf.keras.applications.ResNet50(include_top = False, weights='imagenet', 
                                             input_shape = (config['DATASET']['img_size'], 
                                                            config['DATASET']['img_size'],3))
-    elif model_name == 'inceptionV3':
+    elif 'inceptionV3' in model_name:
         base_model = tf.keras.applications.InceptionV3(include_top = False, weights='imagenet',
                                                             input_shape = (config['DATASET']['img_size'],
                                                             config['DATASET']['img_size'],3))
     
-    elif model_name == 'inception_resnetV2':
+    elif 'inception_resnetV2' in model_name:
         base_model = tf.keras.applications.InceptionResNetV2(include_top = False, weights='imagenet',
                                                             input_shape = (config['DATASET']['img_size'],
                                                             config['DATASET']['img_size'],3))
-    elif model_name == 'xception':
+    elif 'xception' in model_name:
         base_model = tf.keras.applications.xception.Xception(include_top = False, weights='imagenet',
                                                             input_shape = (config['DATASET']['img_size'],
                                                             config['DATASET']['img_size'],3))
-    elif model_name == 'vgg16':
+    elif 'vgg16' in model_name:
         base_model = tf.keras.applications.VGG16(include_top=False, weights="imagenet",
                                                   input_shape=(config['DATASET']['img_size'],
                                                             config['DATASET']['img_size'],3)) 
-    elif model_name == 'efficientnetB0':
+    elif 'efficientnetB0' in model_name:
         base_model = tf.keras.applications.EfficientNetB0(include_top=False, weights="imagenet",
                                                         input_shape=(config['DATASET']['img_size'],
                                                              config['DATASET']['img_size'],3))
-    # if 2nd step of two-step transfer learning process
-    else:
-        model = tf.keras.models.load_model(config['MODEL']['tune_path'])
-    
 
-    # freeze layers of base_model (step 1) or make layers trainable for tuning (step 2) 
-    if config['TRAIN']['tune'] == False:
-        base_model.trainable = False
-        for i, layer in enumerate(base_model.layers):
-            #print(i, layer.name,"-", layer.trainable)
+    ### Transfer learning - step 1
+    print('########### Transfer learning - step 1 ###########')
+    # freeze layers of model (step 1)
+    base_model.trainable = False
+    
+    # define model architecture
+    inp = tf.keras.Input((config['DATASET']['img_size'], config['DATASET']['img_size'], 3))
+    if config['TRAIN']['augment'] == True:
+        x = data_augmentation(inp)
+        x = base_model(x, training=False)
     else:
-        for layer in model.layers:
-            layer.trainable = True
-        for i, layer in enumerate(model.layers):
-            print(i, layer.name,"-", layer.trainable)
+        x = base_model(inp, training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    #x = tf.keras.layers.Dense(256, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    out = tf.keras.layers.Dense(config['DATASET']['num_class'], activation='softmax')(x)
+    model = tf.keras.Model(inp, out)
+   
+    # compile the model
+    if config['TRAIN']['optimizer'] == "SGD":
+        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.02, momentum=0.9,
+                weight_decay=0.0001),
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+    else:
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=config['TRAIN']['lr']),
+                loss='categorical_crossentropy',
+                metrics=['accuracy']) 
 
-    
-    # define model architecture if step 1
-    if config['TRAIN']['tune'] == False:
-        inp = tf.keras.Input((config['DATASET']['img_size'], config['DATASET']['img_size'], 3))
-        if config['TRAIN']['augment'] == True:
-            x = data_augmentation(inp)
-            x = base_model(x, training=False)
-        else:
-            x = base_model(inp, training=False)
-        x = tf.keras.layers.GlobalAveragePooling2D()(x)
-        #x = tf.keras.layers.Dense(256, activation='relu')(x)
-        x = tf.keras.layers.Dropout(0.3)(x)
-        x = tf.keras.layers.Dense(128, activation='relu')(x)
-        x = tf.keras.layers.Dense(64, activation='relu')(x)
-        out = tf.keras.layers.Dense(config['DATASET']['num_class'], activation='softmax')(x)
-        model = tf.keras.Model(inp, out)
-        
-        # compile the model
-        if config['TRAIN']['optimizer'] == "SGD":
-            model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.02, momentum=0.9,
-                    weight_decay=0.0001),
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'])
-        else:
-            model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=config['TRAIN']['lr']),
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy']) 
-    
-    print(model.summary())
-        
     # create checkpoint and early stopping 
     checkpoint = tf.keras.callbacks.ModelCheckpoint(
-                filepath='checkpoints/' + model_name + '_{epoch:03d}-{accuracy:.3f}-{val_accuracy:.3f}.h5',
+                filepath=config['MODEL']['ckpt_path'] + model_name + '_{epoch:03d}-{accuracy:.3f}-{val_accuracy:.3f}',
                 monitor='val_accuracy',
                 verbose=1,
                 save_best_only=True,
@@ -193,13 +180,14 @@ if __name__ == '__main__':
 
     early_stop = tf.keras.callbacks.EarlyStopping(
                 monitor='val_loss', 
-                patience=10,
+                patience=config['TRAIN']['es'],
                 mode='min', 
                 verbose=1) 
     
+    print(model.summary())
     
     # train model
-    print('Training ',  config['MODEL']['name'])
+    print('########### Training',  config['MODEL']['name'], '###########')
     batch_size = config['TRAIN']['batch_size']
     num_epoch = config['TRAIN']['num_epoch']
     train_steps = X_train.shape[0] // batch_size
@@ -219,18 +207,46 @@ if __name__ == '__main__':
                         epochs=num_epoch,
                         callbacks=[checkpoint, early_stop],
                         verbose=1) 
-   
+
+
+    #### Transfer learning - step 2 ####
+    print('########### Transfer learning - step 2 ###########')
+    '''
+    latest = tf.train.latest_checkpoint(config['MODEL']['ckpt_path'])
+    print('Latest checkpoint to be loaded:', latest)
+    model.load_weights(latest)
+    '''
+    model2 = model
+    # make layers trainable for tuning
+    for layer in model2.layers:
+        layer.trainable = True 
+
+    model2.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=config['TRAIN']['lr_tune']),
+                            loss='categorical_crossentropy',
+                            metrics=['accuracy'])
+    print(model2.summary())
+
+    # fit model
+    history = model2.fit(X_train,
+                        y_train,
+                        steps_per_epoch=train_steps,
+                        validation_data=(X_val, y_val),
+                        validation_steps=val_steps,
+                        batch_size=batch_size,
+                        epochs=num_epoch,
+                        callbacks=[checkpoint, early_stop],
+                        verbose=1)  # model performance 
+
     # save model
-    print('Saving to ', config['MODEL']['model_path'])
-    model.save(filepath=config['MODEL']['model_path'], save_format="h5")
-    
-    
-    # model performance 
+    print('########### Saving to:', config['MODEL']['model_path'], '###########')
+    model2.save(filepath=config['MODEL']['model_path'], save_format="h5")
+
+    '''  
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
     loss = history.history['loss']
     val_loss = history.history['val_loss']
-
+  
     # plot: train/val accuracy 
     plt.figure(figsize=(10, 16))
     plt.rcParams['figure.figsize'] = [16, 9]
@@ -255,5 +271,5 @@ if __name__ == '__main__':
     plt.xlabel('epoch')
     plt.tight_layout(pad=3.0)
     plt.savefig(config['MODEL']['plots_path']+config['MODEL']['name']+'_loss')
-
+    '''
 
